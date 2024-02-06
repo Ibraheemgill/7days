34dbd22d1986ce60c580f2a89e3b27073122a29a
ibraheemgill <ibraheemgill63@gmail.com>
2024-02-06 17:25:27 +0500

    seconed commit

---
+++ day1/index.js
@@ -0,0 +1,19 @@
+// var x = 10;
+// console.log(x);
+// "use strict";
+// y = 12;
+// console.log(c);
+// var c = 5;
+function h() {
+    // var d = 45;
+    let e = 8;
+    if (1 == 1) {
+        var d = 45;
+        let e = 4;
+        console.log("from condition " + e);
+
+    }
+    console.log(d);
+    console.log(e);
+}
+h();
